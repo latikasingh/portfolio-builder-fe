3d014499b36439fa7d57bf17d6a311a696065bd3
latika <singhlatika550@gmail.com>
2024-12-31 18:12:14 +0530

    environment removed

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  BASE_URL: 'https://dgrq1jq2-3000.inc1.devtunnels.ms',
-  APP_URL: 'http://localhost:4200',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  BASE_URL: 'https://portfolio-builder-be.onrender.com',
-  APP_URL: 'https://portfolio-builder-fe.vercel.app/',
-};
