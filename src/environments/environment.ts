2e10145bb2219a484fb65e01e9a67338721d69a2
latika <singhlatika550@gmail.com>
2024-12-31 18:16:56 +0530

    env file updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  BASE_URL: 'https://dgrq1jq2-3000.inc1.devtunnels.ms',
-  APP_URL: 'http://localhost:4200',
+  BASE_URL: 'https://portfolio-builder-be.onrender.com',
+  APP_URL: 'https://portfolio-builder-fe.vercel.app/',
 };
